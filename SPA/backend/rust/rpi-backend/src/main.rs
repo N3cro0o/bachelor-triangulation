ff28cfd8e1f8163c9064790373b2fb191883d739
N3cro0o <calko.n3cro0o@gmail.com>
2025-10-19 21:14:30 +0200

    Basic rust backend

--- SPA/backend/rust/rpi-backend/src/main.rs
+++ SPA/backend/rust/rpi-backend/src/main.rs
@@ -2,28 +2,19 @@ use tide::Request;
 use tide::prelude::*;
 
 use std::time;
+use std::sync::Mutex;
 
 use rppal::pwm::{Channel, Polarity, Pwm, Result};
+use rppal::gpio::Gpio;
 
 const PORT: u32 = 8800;
+const SERVO_PORT_0: u8 = 14;
 const PERIOD_MS: u64 = 20; // ms
 const PULSE_0: u64 = 500; // μs
 const PULSE_90: u64 = 1450; // μs
 const PULSE_180: u64 = 2400; // μs
 
-#[derive(Clone)]
-struct ServoState {
-	pub servo: Pwm,
-}
-
-impl ServoState{
-	pub fn default() -> ServoState {
-		ServoState {
-			servo: Pwm::with_period(Channel::Pwm0, time::Duration::from_millis(PERIOD_MS),
-					time::Duration::from_micros(PULSE_0), Polarity::Normal, false);
-		}
-	}
-}
+static servo0_value: Mutex<u64> = Mutex::new(PULSE_90);
 
 fn translate_angle(angle: isize) -> u64 {
 	let one: f32 = (PULSE_180 - PULSE_0) as f32 / 180.0;
@@ -38,20 +29,29 @@ fn translate_angle(angle: isize) -> u64 {
 	return (angle as f32 * one) as u64 + PULSE_0;
 }
 
+fn servo_thread() {
+	let mut servo_pin = Gpio::new().unwrap().get(SERVO_PORT_0).unwrap().into_output();
+	let mut pulse = PULSE_0;
+	loop {
+        {
+		    let data = servo0_value.lock().unwrap();
+		    if pulse != *data {
+			    pulse = *data;
+			    println!("New pulse: {pulse}");
+			    servo_pin.set_pwm(time::Duration::from_millis(PERIOD_MS), time::Duration::from_micros(pulse));
+                std::thread::sleep(time::Duration::from_millis(10));
+		    }
+        }
+	}
+}
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-	let mut state = ServoState::default();
-	state.servo = Pwm::with_period(Channel::Pwm0, time::Duration::from_millis(PERIOD_MS),
-			time::Duration::from_micros(PULSE_90), Polarity::Normal, true);
-    let mut app = tide::with_state(state);
+	let thread_handle = std::thread::spawn(servo_thread);	
+	let mut app = tide::new();
 	app.at("/").get(check_connection);
 	app.at("/servo/:which/set-angle/:angle").get(move_servo);
-	app.at("/servo/reset").get(|req: Request<()>| async move {
-		println!("Reset both servo angles");
-		req.state.servo.set_pulse_width(time::Duration::from_micros(PULSE_90));
-		// add second servo
-		Ok(String::from("Operation done!").into())
-	});
+	app.at("/servo/reset").get(reset_servo);
 	println!("Listening on localhost:{PORT}");
 	app.listen(format!("127.0.0.1:{PORT}")).await?;
 	Ok(())
@@ -66,6 +66,15 @@ async fn move_servo (req: Request<()>) -> tide::Result {
 	let servo_req: String = String::from(req.param("which")?);
 	let angle = translate_angle(angle_req);
 	
-	servo0.set_pulse_width(time::Duration::from_micros(angle));
+	let mut data = servo0_value.lock().unwrap();
+	*data = angle;
+	
 	Ok(format!("Moved servo to angle: {angle}").into())
+}
+
+async fn reset_servo (req: Request<()>) -> tide::Result {
+	println!("Reset both servo angles");
+    let mut data = servo0_value.lock().unwrap();
+    *data = PULSE_90;
+	Ok(String::from("Operation done!").into())
 }
